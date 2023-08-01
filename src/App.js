a5cc3926667d7dd7641a232826fe2d4e839c64b1
Mohamed Yusuf <study@MohamedYusufs-MacBook-Pro.local>
2023-08-01 07:10:17 +0300

    mock api done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 
+//const axios = require('axios/dist/browser/axios.cjs');
+
+import * as cheerio from 'cheerio';
+
 function App() {
+  
+//const axios = require('axios').default; 
+const cheerio = require('cheerio'); 
+ 
+function getInfo(){
+  let address = 'https://www.property24.co.ke/property-for-sale-in-nairobi-p95';
+  let returnJSON = [];
+  
+  fetch(address)
+    .then(function(response) {
+        return response.text();
+    })
+    .then(function(html) {
+        const $ = cheerio.load(html) 
+        let $first = $('div.sc_listingTileContent:first').html();
+        let $A = cheerio.load($first);
+        let firstAnswer = $A('div.sc_listingTileTeaser').text();
+        returnJSON.push({first: firstAnswer});
+        console.log(returnJSON);
+    });
+ 
+
+  return returnJSON;
+}
+
+  getInfo();
   return (
     <div className="App">
       <header className="App-header">
