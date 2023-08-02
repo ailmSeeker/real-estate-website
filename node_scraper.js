d1e27a12942b1a92a9c79a62772038d805b8d52b
Mohamed Yusuf <134999266+ailmSeeker@users.noreply.github.com>
2023-08-02 16:25:11 +0300

    Create node_scraper.js

---
+++ node_scraper.js
@@ -0,0 +1,36 @@
+const axios = require('axios'); 
+const cheerio = require('cheerio');
+const url = 'https://www.property24.co.ke/property-for-sale-in-nairobi-p95';
+const { det, opts, version, Opts, Res } = require("detergent");
+// sc_panel js_listingTile sc_listingTile 
+
+async function getInfo(){
+  let info = []; 
+  
+  let returny = await axios.get(url)  
+  .then(({ data }) => {  
+    const $ = cheerio.load(data); 
+   
+   $('.sc_panel.js_listingTile').each(function(){
+     let flashCardHTML = $(this).find('div.pull-left').html();   
+     let loadFlash = cheerio.load(flashCardHTML);
+     //image gets the title
+     let titleGetter = loadFlash('div.sc_listingTileArea');
+     let priceGetter = loadFlash('div.sc_listingTilePrice span'); 
+     let locationGettere = loadFlash('div.sc_listingTileAddress');
+     let description = loadFlash('div.sc_listingTileTeaser');
+     
+     let title = titleGetter.text();
+
+
+     info.push({'title': det(title).res, 'price': det(priceGetter.text()).res, 'location': det(locationGettere.text()).res, 'description': det(description.text()).res});
+   });
+  
+     return info;
+   });
+  
+}
+
+console.log(getInfo());
+
+getInfo('nairobi-west')
