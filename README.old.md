1c06d45299e3618180a6fe0c10261d0800eabdcf
Mohamed Yusuf <mohamed208.y@proton.me>
2023-08-03 12:12:12 +0300

    login

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# Real_Estate_Website
-To create a successful real estate website to enable buyers to find affordable houses and sellers to get clients to purchase from them.
--- src/App.js
+++ src/App.js
@@ -1,54 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import GetInfo from './getInfo.js'
+import Login from './GetLogin.js'
 
-//const axios = require('axios/dist/browser/axios.cjs');
-
-import * as cheerio from 'cheerio';
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
-  
-//const axios = require('axios').default; 
-const cheerio = require('cheerio'); 
- 
-function getInfo(){
-  let address = 'https://www.property24.co.ke/property-for-sale-in-nairobi-p95';
-  let returnJSON = [];
-  
-  fetch(address)
-    .then(function(response) {
-        return response.text();
-    })
-    .then(function(html) {
-        const $ = cheerio.load(html) 
-        let $first = $('div.sc_listingTileContent:first').html();
-        let $A = cheerio.load($first);
-        let firstAnswer = $A('div.sc_listingTileTeaser').text();
-        returnJSON.push({first: firstAnswer});
-        console.log(returnJSON);
-    });
- 
-
-  return returnJSON;
-}
-
-  getInfo();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>hello</h1>
+    <ChakraProvider>
+        <Login />
+    </ChakraProvider>
+    </>
   );
 }
 
