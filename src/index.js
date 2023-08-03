4379f40fdbd2b1c944e838e3d76b028ec3feb715
Slonetech <kengeregordon@gmail.com>
2023-08-03 20:51:55 +0300

    merged the changes

--- src/index.js
+++ src/index.js
@@ -1,35 +1,18 @@
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-
-import App from './contact';
-
 import LoginSection from './Components/LoginSection';
-import reportWebVitals from './reportWebVitals';
-import 'mdb-react-ui-kit/dist/css/mdb.min.css';
-import "@fortawesome/fontawesome-free/css/all.min.css";
 
-const root =ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-
-  <BrowserRouter>
-  <Routes>
-  <Route path="/" element={  <App />}></Route>
-  <Route path="/login" element={  <LoginSection />}></Route>
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<LoginSection />} />
+      </Routes>
     </BrowserRouter>
-    <App />
-  </React.StrictMode>
-)
+  </React.StrictMode>,
+  document.getElementById('root')
+);
